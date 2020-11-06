a3dcde61ec78818202fa7d5b9ed3d7c176a852ef
enfrte <enfrte@gmail.com>
2020-11-06 20:01:57 +0200

    Added styles for index

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/assets/styles/main.css'
 
 Vue.config.productionTip = false
 
