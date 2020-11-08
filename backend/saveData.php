a10d73d86707722e6eb5092f09395e192d795413
enfrte <enfrte@gmail.com>
2020-11-08 03:16:24 +0200

    Added more form elements, slug checker, form styles

--- backend/saveData.php
+++ backend/saveData.php
@@ -1,22 +1,13 @@
 <?php 
-
-// Saves data to a new json database defined by a timestamp
+// Saves data to a new json object database - defined by a timestamp
 
 $timestamp = time();
 $filename = 'database/' . date("Ymdhis",$timestamp).'-practice-data.json';
 
 //$data = json_decode(file_get_contents('php://input'), true);
 //echo json_encode($data);
-$data = file_get_contents('php://input'); // see above two lines
+$data = file_get_contents('php://input'); 
 
 file_put_contents($filename, $data);
 
-//echo '{"output": "'.gettype($data).'"}';
-echo $data;
-
-//echo json_decode($_POST);
-//echo $timestamp;
-
-//$json_example = '{"abc": 123, "def": "456"}';
-
-//$json 
+echo $data; // or perhaps return?
--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -4,15 +4,49 @@
         <div v-if="jsonData" class="">
             <div v-for="data in jsonData" :key="data._id" class="card mt-4">
                 <div class="card-body">
-                    <h2 class="card-title">{{ data.title }}</h2>
-                    <div class="form-group">
-                        <label for="" class="mr-2">Title </label>
-                        <input :id="'title_'+data._id" type="text" v-model="data.title" placeholder="Edit me">
+                    <div class="form-group row">
+                        <div class="col-sm-10 offset-sm-2">
+                            <h2 class="card-title">{{ data.title }}</h2>
+                        </div>
                     </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">Checkbox</div>
+                        <div class="col-sm-10">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" :id="'inProduction_'+data._id" v-model="data.inProduction">
+                                <label class="form-check-label" for="">Published </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Title </label>
+                        <div class="col-sm-10">
+                            <input :id="'title_'+data._id" type="text" v-model="data.title" class="form-control" placeholder="Edit me">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Slug </label>
+                        <div class="col-sm-10">
+                            <input v-on:keyup="slugCheck" :value="data.slug" :id="'slug_'+data._id" type="text" class="form-control"   placeholder="Slug link">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Tutorial </label>
+                        <div class="col-sm-10">
+                            <textarea :id="'Tutorial_'+data._id" v-model="data.tutorial" class="form-control textarea-tutorial" placeholder="Markdown supported"></textarea>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <div class="col-sm-10 offset-sm-2">
+                            <button :id="'addQuestion_'+data._id" class="btn btn-primary" type="button" v-on:click="addQuestion">Add question</button>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
-        <div v-else><h1>Data not loaded. Check if Apache is running</h1></div>
+        <div v-else><h3 class="text-warning">Data is loading, or data has not loaded. <br>Check if Apache is running</h3></div>
 
         <div class="mt-4">
             <button class="btn btn-primary m-1" type="button" v-on:click="addLesson">Add lesson</button>
@@ -31,7 +65,7 @@
             }
         },
         computed: {
-            
+
         },
         async created() {
             const databaseFile = await this.getLatestDatabase();
@@ -40,21 +74,48 @@
             //console.log(this.jsonData);
         },
         methods: {
+            slugCheck (e) {
+                let slugId = e.target.id
+                const slugText = e.target.value
+                slugId = slugId.replace('slug_','')
+                const dataObjIndex = this.jsonData.findIndex((val) => {
+                    return val._id === slugId
+                });
+                //this.jsonData[dataObjIndex].slug = slugText
+                //console.log("dataObj", dataObjIndex); 
+                const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿỳýœæŕśńṕẃǵǹḿǘẍźḧ'
+                const b = 'aaaaeeeeiiiioooouuuuncsyyyoarsnpwgnmuxzh'
+                const p = new RegExp(a.split('').join('|'), 'g')
+
+                this.jsonData[dataObjIndex].slug = slugText.toString().toLowerCase()
+                    .replace(/[\s_]+/g, '-')
+                    .replace(p, c =>
+                    b.charAt(a.indexOf(c)))
+                    .replace(/&/g, `-and-`)
+                    .replace(/[^\w-]+/g, '')
+                    .replace(/--+/g, '-')
+                    .replace(/^-+|-+$/g, '')
+                console.log("jsonData", this.jsonData);
+            },
+            addQuestion() {
+                console.log("addQuestion called");
+            },
             addLesson() {
                 this.jsonData.push(
                     {
                         "_id": this.generateUUID(), 
                         "title": "",
-                        /* to add
-                        "inProduction":true,
-                        "slug":"verb-chaining",
-                        */
+                        "inProduction": false,
+                        "slug": "",
+                        "tutorial": "",
+                        "lang": "Finnish", // hard coded for now, but in futire set from a config file
+                        "questions": [], // an array of objects like this - { "en": [""], "fi": [""], "_id": "" }
                     }
                 );
                 //console.log("Lesson added", this.jsonData);
             },
             submitForm() {
-                // Submit for changes to the backend - PHP will process it and save a copy
+                // Submit changes to the backend - PHP will process it and save a copy
                 // Requires Apache server running on port 80
                 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
                 fetch('http://localhost/expression-practice-admin-vue/backend/saveData.php', {
@@ -103,5 +164,18 @@
 <style scoped>
     .admin {
         background-color: #333;
+    }
+
+    .textarea-tutorial-container {
+        max-width: 600px;
+        margin: auto;
+    }
+
+    .form-elements-container {
+        max-width: 600px;
+    }
+
+    .textarea-tutorial {
+        height: 400px;
     }
 </style>
